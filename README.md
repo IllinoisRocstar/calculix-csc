926e812888a79e9738244d7f015eacea94fd7e64
Masoud Safdari <msafdari@illinoisrocstar.com>
2019-12-06 15:32:28 -0600

    initial commit with cmake build system and skeletton CSC module.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# calculix-csc
+CalculiX -- IR Extras
+---------------------
+This is plain, vanila CalculiX project (v2.15) with a few additions as follows:
+* IMPACT coupling module (CSC)
+* Enhanced build system (based on CMake)
+* CUDA based solvers (currently in-active) 
+* ExodusII output format 
+The CUDA and ExodusII support is made available using patches released by Peter A. Gustafson (last update 8/14/2019).
+
+## Version
+Version 2.15.1
+
+The project follows the versioning convention of the original CalculiX. The versioning will be major.minor.patch and increaments are as following:
+* major: shows major version of the CalculiX project 
+* minor: shows minor version of the CalculiX project
+* patch: will be increamented as we release more functionality to the project.
+
+## Options
+The following table contains all available CMake options to configure the project. The necessary third-party libraries are listed in the notes section.
+
+| Option name            | Option description              | Default | Notes                            |
+|------------------------|---------------------------------|---------|----------------------------------|
+| ENABLE_PAR             | Enable MPI support              | OFF     | Requires MPI compiler            |
+| ENABLE_CSC         | Enable coupling client                  | ON      |                                  |
+
+## Unix Building Instructions ##
+Poject requires followin depencies (on Ubuntu):
+
+* libspooles-dev
+* libexodusii-dev
+* IMPACT
+
+For instructions regarding building IMPACT please refer to [here](https://github.com/IllinoisRocstar/IMPACT).
+
+You can compile the project:
+
+```
+export IMPACT_INSTALL_PATH=full_path_to_impact_install
+mkdir build
+cd build
+cmake .. \
+    -DCMAKE_PREFIX_PATH=${IMPACT_INSTALL_PATH} \
+make -j$(nproc) (or however many threads you'd like to use)
+$ make install (sudo if install location requires it)
+```
 
---
+++ src/csc/clcxDRV.C
@@ -0,0 +1,117 @@
+#include "com.h"
+#include "com_devel.hpp"
+#include "mpi.h"
+#include <iostream>
+
+COM_EXTERN_MODULE(clcxcsc);
+
+// static vars
+int _wrank = 0;
+int _wsize = 0;
+int _vrb = 0;
+int _hndl;
+std::string _jn = "case_name";
+
+// aux declarations
+void usage(char **);
+void exitme(std::string msg = std::string());
+void procArgs(int argc, char *arg[]);
+
+// main driver program
+main(int argc, char *argv[]) {
+
+  // MPI communicator setup
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &_wsize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &_wrank);
+
+  // sanity checking
+  if (argc < 2)
+    usage(argv);
+  else
+    procArgs(argc, argv);
+
+  // initializing COM
+  COM_init(&argc, &argv);
+
+  // loading
+  COM_LOAD_MODULE_STATIC_DYNAMIC(clcxcsc, "clcx");
+
+  // set job name
+  _hndl = COM_get_function_handle("clcx.set_jobName");
+  if (_hndl < 0)
+    exitme("Error obtaining set_jobName handle");
+  COM_call_function(_hndl, 1, _jn);
+
+  // initialize
+  _hndl = COM_get_function_handle("clcx.initialize");
+  if (_hndl < 0)
+    exitme("Error obtaining initialize handle.\n");
+  COM_call_function(_hndl, 1, _vrb);
+
+  // run
+  _hndl = COM_get_function_handle("clcx.run");
+  if (_hndl < 0)
+    exitme("Error obtaining run handle.\n");
+  COM_call_function(_hndl);
+
+  // finalize
+  _hndl = COM_get_function_handle("clcx.finalize");
+  if (_hndl < 0)
+    exitme("Error obtaining finalize handle.\n");
+  COM_call_function(_hndl);
+
+  // unloading
+  COM_UNLOAD_MODULE_STATIC_DYNAMIC(clcxcsc, "clcx");
+
+  // finalizing COM
+  COM_finalize();
+
+  // peaceful exit
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Finalize();
+  return 0;
+}
+
+// aux implementations
+void usage(char *argv[]) {
+  if (_wrank == 0) {
+    std::cout << "Calculix Component Side Client (CSC) Driver" << std::endl;
+    std::cout << "NOTE: This is an experimental interface to the Calculix and "
+                 "subject to change\n";
+    if (_wsize > 1)
+      std::cout << "Parallel run with " << _wsize << " processes.\n";
+    std::cout << std::endl << "Usage: " << std::endl;
+    std::cout << "\t" << argv[0] << " [[-flags] [case_name]]"
+              << "\n\nwhere flags are " << std::endl
+              << "\t-v : "
+              << "verbose output" << std::endl
+              << "\t-e : "
+              << "exodus output" << std::endl
+              << "\t-c : "
+              << "case file name [case_name]" << std::endl
+              << std::endl
+              << std::endl;
+  }
+  exitme("Completed!");
+}
+
+void exitme(std::string msg) {
+  if (_wrank == 0) {
+    std::cerr << "Calculix-Driver : " << msg << std::endl;
+  }
+  MPI_Finalize();
+  exit(-1);
+}
+
+void procArgs(int argc, char *argv[]) {
+  if (argc > 2) {
+    for (int iArg = 1; iArg < argc; iArg++) {
+      std::string args(argv[iArg]);
+      if (args.compare("-c") == 0)
+        _jn = std::string(argv[iArg + 1]);
+      if (args.compare("-v") == 0)
+        _vrb = 1;
+    }
+  }
+}
